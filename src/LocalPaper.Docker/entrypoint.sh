77308151c96df4e88b2dd3d9b162144dad9fc56e
Josip Medved <jmedved@jmedved.com>
2025-05-26 14:20:34 -0700

    Config directory is filled from example, if empty

--- src/LocalPaper.Docker/entrypoint.sh
+++ src/LocalPaper.Docker/entrypoint.sh
@@ -3,4 +3,8 @@ SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 if [ "$TERM" = "xterm" ]; then TERM=xterm-256color; fi
 
+if [ -z "$(ls -A "/config")" ]; then
+    cp -r "$SCRIPT_DIR/example/"* "/config/" 2>/dev/null || true
+fi
+
 "$SCRIPT_DIR/bin/LocalPaper" "$@"
