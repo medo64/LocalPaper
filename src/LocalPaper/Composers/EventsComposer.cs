afd08133aa752525e384829cd1eeaedb9fb54f1d
Josip Medved <jmedved@jmedved.com>
2025-05-25 21:13:38 -0700

    Suppressing same event values

--- src/LocalPaper/Composers/EventsComposer.cs
+++ src/LocalPaper/Composers/EventsComposer.cs
@@ -27,15 +27,20 @@ internal sealed class EventsComposer : IComposer {
 
         var y = (int)(clipRect.Top - fontText.Metrics.Ascent);
         var lastKey = string.Empty;
+        var lastValue = string.Empty;
         foreach (var kvp in Helpers.GetConfigEntries(Directory, DateOnly.FromDateTime(time.AddSeconds(Offset.TotalSeconds)))) {
-            if (!kvp.Key.Equals(lastKey)) {
+            if (!kvp.Key.Equals(lastKey, StringComparison.Ordinal)) {
                 if (!string.IsNullOrEmpty(lastKey)) { y += (int)clipRect.Top; }
                 canvas.DrawText(kvp.Key, clipRect.Left, y, SKTextAlign.Left, fontHead, paint);
                 y += (int)(fontText.Metrics.Descent - fontText.Metrics.Ascent);
                 lastKey = kvp.Key;
+                lastValue = string.Empty;
             }
+
+            if (kvp.Value.Equals(lastValue, StringComparison.Ordinal)) { continue; }  // skip if same
             canvas.DrawText(kvp.Value, clipRect.Left, y, SKTextAlign.Left, fontText, paint);
             y += (int)(fontText.Metrics.Descent - fontText.Metrics.Ascent);
+            lastValue = kvp.Value;
         }
     }
 
