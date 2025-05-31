01bffc51b9723d7e7388b85584b4666d66df581b
Josip Medved <jmedved@jmedved.com>
2025-05-31 11:43:45 -0700

    Updated entrypoint script

--- src/LocalPaper.Docker/entrypoint.sh
+++ src/LocalPaper.Docker/entrypoint.sh
@@ -5,6 +5,9 @@ if [ "$TERM" = "xterm" ]; then TERM=xterm-256color; fi
 
 if [ -z "$(ls -A "/config")" ]; then
     cp -r "$SCRIPT_DIR/example/"* "/config/" 2>/dev/null || true
+    if [ ! -z "$(ls -A "/config")" ]; then
+        echo "Copied example configuration to /config."
+    fi
 fi
 
 "$SCRIPT_DIR/bin/LocalPaper" "$@"
