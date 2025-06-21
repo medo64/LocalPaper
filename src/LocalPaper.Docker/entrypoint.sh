3780ce425beb9b983a419970694a167e21ddcda2
Josip Medved <jmedved@jmedved.com>
2025-06-20 21:05:58 -0700

    Refactoring entrypoint and healthcheck

--- src/LocalPaper.Docker/entrypoint.sh
+++ src/LocalPaper.Docker/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 if [ "$TERM" = "xterm" ]; then TERM=xterm-256color; fi
 
+exec 28433> /var/tmp/.entrypoint.lock
+flock -n 28433 || { echo -e "\033[91mScript is already running\033[0m" >&2 ; exit 113; }
+
 if [ -z "$(ls -A "/config")" ]; then
-    cp -r "$SCRIPT_DIR/example/"* "/config/" 2>/dev/null || true
+    cp -r "/app/example/"* "/config/" 2>/dev/null || true
     if [ ! -z "$(ls -A "/config")" ]; then
         echo "Copied example configuration to /config."
     fi
 fi
 
-"$SCRIPT_DIR/bin/LocalPaper" "$@"
+/app/LocalPaper "$@"
