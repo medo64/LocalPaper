68a859f34ce7e5d0bf915b092ef114c64afd921b
Josip Medved <jmedved@jmedved.com>
2025-11-21 07:04:02 -0800

    Replacing flock usage

--- src/LocalPaper.Docker/entrypoint.sh
+++ src/LocalPaper.Docker/entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
+SCRIPT_NAME=`basename $0`
 
 if [ "$TERM" = "xterm" ]; then TERM=xterm-256color; fi
 
-exec 28433> /var/tmp/.entrypoint.lock
-flock -n 28433 || { echo -e "\033[91mScript is already running\033[0m" >&2 ; exit 113; }
+PID_FILE="/var/run/.$SCRIPT_NAME.pid"
+if [ -e "$PID_FILE" ]; then
+    PID_LAST=$(cat "$PID_FILE")
+    if ps -axo pid | grep -q "^ *$PID_LAST\$"; then
+        echo "${ANSI_RED}$SCRIPT_NAME: script is already running!${ANSI_RESET}" >&2
+        exit 255
+    fi
+fi
+echo $$ > $PID_FILE
+trap "rm $PID_FILE 2>/dev/null" 0
+trap "exit 113" INT TERM
 
 if [ -z "$(ls -A "/config")" ]; then
     cp -r "/app/example/"* "/config/" 2>/dev/null || true
